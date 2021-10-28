c0c4303a75626fe49e39d79abf392beb10938db0
juliocezar96 <juliocr.jc38@gmail.com>
2021-10-28 16:15:10 -0300

    first commit
    Criação da base do jogo, campo Field

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import {StyleSheet, Text, View } from 'react-native';
+
+import Field from './src/components/Field';
+import params from './src/params';
+
+
+export default class App extends Component{
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines!</Text>
+        <Text style={styles.instructions}>Tamanho da grade: 
+          {params.getRowsAmount()}x{params.getColumnsAmount()}
+        </Text>
+
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+
+  },
+  welcome:{
+
+  },
+  instructions:{
+    
+  }
+})
