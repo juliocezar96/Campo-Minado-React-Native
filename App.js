c2acde1ad80e40a9d76d2403588ef06dce79c00b
juliocezar96 <juliocr.jc38@gmail.com>
2021-11-11 19:58:06 -0300

    implementação do tabuleiro

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 import {StyleSheet, Text, View } from 'react-native';
 
+import { createMinedBoard } from './src/functions';
+import MineField from './src/components/MineField';
 import Flag from './src/components/Flag';
 import Field from './src/components/Field';
 import params from './src/params';
 
 
 export default class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = this.createState();
+  }
+
+  //calcula quantidade de minas dentro do tabuleiro e percentua a dificuldade 
+  minesAmount = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+
+    return {
+      board : createMinedBoard(rows, cols, this.minesAmount()),
+    }
+
+  }
+
   render(){
     return(
       <View style={styles.container}>
@@ -15,20 +41,10 @@ export default class App extends Component{
           {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
 
-        <Flag />
-        <Flag bigger/>
-
-        <Field />
-        <Field opened />
-        <Field opened nearMines={1} />
-        <Field opened nearMines={2} />
-        <Field opened nearMines={3} />
-        <Field opened nearMines={6} />
-        <Field mined />
-        <Field mined opened />
-        <Field mined opened  exploded/>
-        <Field flagged/>
-        <Field flagged opened/>
+        <View style={styles.board}>
+          <MineField board={this.state.board} />
+        </View>
+      
       </View>
     );
   }
@@ -37,10 +53,12 @@ export default class App extends Component{
 const styles = StyleSheet.create({
   container:{
     flex: 1,
-    justifyContent: 'center',
-    justifyContent:'space-around',
-    alignItems:'center',
+    justifyContent:'flex-end',
     
+  },
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA'
   },
   welcome:{
 
--- src/functions.js
+++ src/functions.js
@@ -2,8 +2,8 @@ const createBoard = (rows, columns) => {
     return Array(rows).fill(0).map((_, row) => {
         return Array(columns).fill(0).map((_, column) => {
             return {
-                row: row,
-                column: column,
+                row,
+                column,
                 opened: false,
                 flagged: false,
                 mined: false,
@@ -17,28 +17,25 @@ const createBoard = (rows, columns) => {
 //espalha as minas no tabuleiro
 const spreadMines = (board, minesAmount) => {
 
-    const rows = board.lenght;
-    const columns = board[0].lenght;
+    const rows = board.length;
+    const columns = board[0].length;
     let minesPlanted = 0;
     
-    while(minesPlanted < minesAmount){
-        const rowSelec = parseInt(Math.random() * rows, 10);
-        const columnSelec = parseInt(Math.random() * columns, 10);
+    while(minesPlanted < minesAmount) {
+        const rowSelec = parseInt(Math.random() * rows, 10)
+        const columnSelec = parseInt(Math.random() * columns, 10)
 
-        if(!board[rowSelec][columnSelec].mined){
-            board[rowSelec][columnSelec.mined] = true;
-            minesPlanted++;
+        if (!board[rowSelec][columnSelec].mined) {
+            board[rowSelec][columnSelec].mined = true
+            minesPlanted++
         }
     }
 }
 
-//criação do tabuleiro com as meinas plantadas
-const createMinedBoard =(rows, columns, minesAmount) => {
-
-    const board = createBoard(rows, columns);
-    spreadMines(board,minesAmount);
-
-    return board;
+const createMinedBoard = (rows, columns, minesAmount) => {
+    const board = createBoard(rows, columns)
+    spreadMines(board, minesAmount)
+    return board
 }
 
 export {createMinedBoard}
